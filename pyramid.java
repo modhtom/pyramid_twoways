9b48a92179b0dfcfd5c65af8ca36ba57826f1129
modhtom <85494929+modhtom@users.noreply.github.com>
2021-10-07 17:33:30 +0200

    Update pyramid.java

--- pyramid.java
+++ pyramid.java
@@ -1,10 +1,10 @@
 import java.util.Scanner;
 public class pyramid{
 public static void main(String[] args) {
-	Scanner input=new Scanner(System.in);
-	System.out.print("Enter the number of rows:");
-	int r=input.nextInt();
-	int k=0;
+    Scanner input=new Scanner(System.in);
+    System.out.print("Enter the number of rows:");
+    int r=input.nextInt();
+    int k=0;
     for (int i=1;i<=r;++i,k=0){
     for (int s=1;s<=r-i;++s){
     System.out.print("  ");}
